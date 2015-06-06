7a819056ca8d627f0b9715b1bccbe0b57b8ec8ea
krujos <jkruck@pivotal.io>
2015-06-06 11:58:45 -0500

    Failing tests

--- clientcredentials.go
+++ clientcredentials.go
@@ -1,1 +1,23 @@
 package uaaclientcredentials
+
+//UaaClientCredentials provides a token for a given clientId and clientSecret.
+//The token is refreshed for you according to expires_in
+type UaaClientCredentials struct {
+	uaaURI             string
+	clientID           string
+	clientSecret       string
+	authorizationToken string
+	expiresAt          string
+}
+
+//GetBearerToken returns a currently valid bearer token to use against the
+//CF API. You should not cache the token as the library will handle updating
+//it if it's expired.
+func (creds *UaaClientCredentials) GetBearerToken() string {
+	return ""
+}
+
+//New UaaClientCredentials factory
+func New(uaaURI string, clientID string, clientSecret string) (*UaaClientCredentials, error) {
+	return nil, nil
+}
--- uaaclientcredentials_test.go
+++ uaaclientcredentials_test.go
@@ -9,4 +9,24 @@ import (
 
 var _ = Describe("Uaaclientcredentials", func() {
 
+	Describe("Creationism", func() {
+		It("makes an initiliazed object", func() {
+			uaaCC, _ := New("https://uaa.at.your.place.com", "client_id", "client_secret")
+			Expect(uaaCC).NotTo(BeNil())
+		})
+	})
+
+	Describe("Bearer Tokens", func() {
+		var uaaCC *UaaClientCredentials
+
+		BeforeEach(func() {
+			uaaCC, _ = New("https://uaa.at.your.place.com", "client_id", "client_secret")
+		})
+
+		It("should return a properly formatted bearer token", func() {
+			token := uaaCC.GetBearerToken()
+			Expect(token).NotTo(BeNil())
+			Expect(token).NotTo(Equal(""))
+		})
+	})
 })
